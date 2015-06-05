65ddaa67fad09e2e737d09cacbdfb37de9d3bc3f
hadoop_consultants <hadoop@truecaller.com>
2015-06-05 11:05:35 +0200

    fixes

--- src/main/scala/com/truecaller/NewUsersDemo.scala
+++ src/main/scala/com/truecaller/NewUsersDemo.scala
@@ -27,8 +27,8 @@ object NewUsersDemo {
 
   def main(args: Array[String]) {
 
-    if (args.length < 2) {
-      System.err.println("Usage: NewUsersDemo <zookeeper> <topic> ")
+    if (args.length < 3) {
+      System.err.println("Usage: NewUsersDemo <zookeeper> <topic> <batch_interval> ")
       System.exit(1)
     }
 
@@ -37,45 +37,45 @@ object NewUsersDemo {
     // parsing parameters
     val zookeeper = args(0)
     val topic = args(1)
+    val batchInterval = args(2).toInt
 
     // initialization and configuration
     val sparkConf = new SparkConf().setAppName("NewUsersDemo")
-    val ssc = new StreamingContext(sparkConf, Seconds(5))
+    val ssc = new StreamingContext(sparkConf, Seconds(batchInterval))
     val numInputMessages = ssc.sparkContext.accumulator(0L, "Kafka messages consumed")
 
     // connect to Kafka to fetch the log events
     val topicMap = Map(topic -> 1)
 
     val kafkaParams = Map[String, String]("zookeeper.connect" -> zookeeper,
-                                          "group.id" -> "sparkDemo")
+      "group.id" -> "sparkDemo")
 
-    val lines = KafkaUtils.createStream[Array[Byte], Array[Byte], DefaultDecoder, DefaultDecoder] (
+    val lines = KafkaUtils.createStream[Array[Byte], Array[Byte], DefaultDecoder, DefaultDecoder](
       ssc, kafkaParams, topicMap, StorageLevel.MEMORY_AND_DISK).map(_._2)
 
+    val cnt = lines.map{ avroEvent => {
+      val avroLogDecoder = new AvroLogDecoder()
+      avroLogDecoder.init(topic)
+      avroLogDecoder.decode(avroEvent)
+    }}
+      .filter(_.getSearch.getSearchString != null)
+      .map( event => (event.getUser.getRegisterId, event.getSearch.getSearchString))
+      .count()
 
-    lines.foreachRDD { r => {
-      if (r.count() == 0)
-        logger.error("pustooo")
-      else
-      logger.error("cos jest :)")
-      r.foreachPartition(rddPart => {
-        val avroLogDecoder = new AvroLogDecoder()
-        avroLogDecoder.init(topic)
-        val list = rddPart.toList
-        if (list.size == 0) {
-          logger.error("pusto")
-        } else {
-          list.map(event => {
-            logger.error("processing event...")
-            val appEvent = avroLogDecoder.decode(event)
-            logger.error("appEvent: " + appEvent)
-          })
-          logger.error("cos jest: " + list.size)
-        }
+    cnt.print()
 
-      })
-    }
-    }
+//    val appEvents = lines.foreachRDD { r =>
+//    {
+//      r.foreachPartition(rddPart => {
+//        val avroLogDecoder = new AvroLogDecoder()
+//        avroLogDecoder.init(topic)
+//        val list = rddPart.toList
+//
+//        list.map(event => {
+//          avroLogDecoder.decode(event)
+//        })
+//      })
+//    }}
 
     // start streaming process
     ssc.start()
